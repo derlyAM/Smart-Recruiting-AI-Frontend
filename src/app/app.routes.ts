203976f7f562325c0e7f9bb2af95e1652ee889d2
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-07 17:22:12 -0500

    Redireccion aplicada hacia login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => AuthRoutingModule,
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
+];
