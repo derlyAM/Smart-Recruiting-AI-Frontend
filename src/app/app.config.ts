0636d6c4b88a23c5361166adef018a1e58517520
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-08 16:53:11 -0500

    Cors interceptor agregado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,8 @@
-import { ApplicationConfig, Provider, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
-import { DireccionServidorInterceptor } from './shared/interceptors/direccion-servidor.interceptor';
-
-const customHttpInterceptors: Provider[] = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: DireccionServidorInterceptor, multi: true },
-];
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { customHttpInterceptors } from './shared/interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
