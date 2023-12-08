1a6743685f39ed18b6019ea582de5989f4c20b6b
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-08 16:37:44 -0500

    Utilidades de token agregadas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, Provider, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+
+export const authnterceptorProvider: Provider = { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true };
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    authnterceptorProvider,
+  ],
 };
