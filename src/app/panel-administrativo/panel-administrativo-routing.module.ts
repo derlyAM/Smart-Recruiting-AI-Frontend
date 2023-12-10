080caad3567833a84f2c215de14fa3de7c3f152c
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-10 16:30:44 -0500

    Componente basico creado para vacantes recomendadas

--- src/app/panel-administrativo/panel-administrativo-routing.module.ts
+++ src/app/panel-administrativo/panel-administrativo-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { PublicarVacanteComponent } from './publicar-vacante/publicar-vacante.component';
+import { VacantesRecomendadasComponent } from './vacantes-recomendadas/vacantes-recomendadas.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'publicar-vacante',
     component: PublicarVacanteComponent,
+  },
+  {
+    path: 'vacantes-recomendadas',
+    component: VacantesRecomendadasComponent
   },
   {
     path: '**',
---
+++ src/app/panel-administrativo/vacantes-recomendadas/vacantes-recomendadas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vacantes-recomendadas',
+  standalone: true,
+  imports: [],
+  templateUrl: './vacantes-recomendadas.component.html',
+  styleUrl: './vacantes-recomendadas.component.scss'
+})
+export class VacantesRecomendadasComponent {
+
+}
